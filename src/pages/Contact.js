aa5469bc4073dc3309a49c809f275110bd81d5f7
MuhammadAbdulBari <135426818+MuhammadAbdulBari@users.noreply.github.com>
2026-04-14 02:30:04 +0500

    Remove unused useRef import from Contact.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,5 +1,5 @@
 // pages/Contact.js
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import { 
   Phone, Mail, MapPin, MessageCircle, Send, Clock, 
   Facebook, Linkedin, Twitter, Instagram, ArrowRight, CheckCircle,
